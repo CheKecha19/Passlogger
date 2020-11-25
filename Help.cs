d52c917df7ee4478eecf81920fe2cbd8828dffbe
Никита Назаров <chechecherokee@gmail.com>
2020-11-25 19:48:56 +0300

    Create codeql-analysis.yml

---
+++ Help.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Passlogger
+{
+    public partial class Help : Form
+    {
+        public Help()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_Load(object sender, System.EventArgs e)
+        {
+            Activate();
+        }
+    }       
+}
